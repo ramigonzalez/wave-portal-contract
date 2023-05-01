55ce4c167480e40e5852d3db949ed17e3f1df56d
LAPTOP-QOOVCJT1\Dudley Dursley <ramiro.gonzalez@sandbox.game>
2023-05-01 18:22:56 -0300

    wave contract updated to receive messages and update name

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,24 +1,45 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.9;
 
 import "hardhat/console.sol";
 
 contract WavePortal {
 
-    uint256 totalWaves;
+    uint256 public totalWaves;
 
-    constructor() {
-      console.log("Hello web3 world!");
+    mapping(address => string) waverNames;
+
+    struct Wave {
+        address waver;
+        string name;
+        uint256 timestamp;
+        string message;
     }
 
-    function wave() public {
+    Wave[] waves;
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    constructor() {}
+
+    function wave(string memory _message) public {
       totalWaves += 1;
-      console.log("%s deu tchauzinho!", msg.sender);
+      console.log("%s wave us witho following message: %s!", msg.sender, _message);
+      string memory _name = waverNames[msg.sender];
+      waves.push(Wave(msg.sender, _name, block.timestamp, _message));
+      emit NewWave(msg.sender, block.timestamp, _message);
     }
 
-    function getTotalWaves() public view returns (uint256) {
-      console.log("Temos um total de %d tchauzinhos!", totalWaves);
-      return totalWaves;
+    function getAllWaves() public view returns (Wave[] memory) {
+      return waves;
+    }
+
+    function setName(string memory _name) public {
+      waverNames[msg.sender] = _name;
+    }
+
+    function getName() public view returns(string memory) {
+      return waverNames[msg.sender];
     }
 }
